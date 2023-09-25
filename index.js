d8b2ccc4e77f0ca9183262a5ad28f0f0b28f656b
alaska.alex.agp <108288948+malankinAM@users.noreply.github.com>
2023-09-25 22:08:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function sendMail() {
     message: document.getElementById("message").value,
   };
 
-  const serviceID = "service_hm8cr8a";
-  const templateID = "template_5ovd7h5";
+  const serviceID = "service_nb4d5oa";
+  const templateID = "template_sloxsyo";
 
     emailjs.send(serviceID, templateID, params)
     .then(res=>{
